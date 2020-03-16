81da0b4af2c6867cf1fe8080e74480cca5b91bf3
ucha3i <jchabik@gmail.com>
2020-03-16 21:15:13 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Mongo API Project
+
+Simple project using Mongo DB and Mongoose, to seeding a database.
+
+## The problem
+
+... 
+
+## View it live
+
+Live version on Heroku: https://agile-refuge-38314.herokuapp.com/ 
---
+++ server.js
@@ -0,0 +1,125 @@
+import express from 'express'
+import bodyParser from 'body-parser'
+import cors from 'cors'
+import mongoose from 'mongoose'
+
+const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/budget"
+mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.Promise = Promise
+
+// setup mangoose models
+
+const Account = mongoose.model('Account', {
+  name: String,
+  saldo: Number
+})
+
+const Category = mongoose.model('Category', {
+  name: String,
+  description: String
+})
+
+const Expense = mongoose.model('Expense', {
+  amount: Number,
+  date: Date,
+  description: String,
+  category: {
+  type: mongoose.Schema.Types.ObjectId,
+  ref: 'Category'
+  },
+  account: {
+  type: mongoose.Schema.Types.ObjectId,
+  ref: 'Account'
+  }
+})
+
+// ports
+
+const port = process.env.PORT || 8080
+const app = express()
+
+// middlewares to enable cors and json body parsing
+
+app.use(cors())
+app.use(bodyParser.json())
+
+// routes 
+
+app.get('/', (_, res) => {
+  res.send('Budget')
+})
+
+app.get('/accounts', async (_, res) => {
+  res.json(await Account.find())
+})
+
+app.get('/categories', async (_, res) => {
+  res.json(await Category.find())
+})
+
+app.get('/expenses', async (_, res) => {
+  res.json(await Expense.find())
+})
+
+// saving data
+
+app.post('/accounts', async (req, res) => {
+  try {
+    const { name, saldo } = req.body
+    const acc = new Account({ name: name, saldo: saldo })
+    await acc.save()
+
+    res.status(201).json(acc)
+
+  } catch (err) {
+    res
+      .status(400)
+      .json({ message: 'Could not create account', errors: err.errors })
+  }
+})
+
+app.post('/categories', async (req, res) => {
+  try {
+    const { name, description } = req.body
+    const cat = new Category({ name: name, description: description })
+    await cat.save()
+
+    res.status(201).json(cat)
+
+  } catch (err) {
+    res
+      .status(400)
+      .json({ message: 'Could not create category', errors: err.errors })
+  }
+})
+
+app.post('/expenses', async (req, res) => {
+  try {
+    const { amount, date, description, category, account } = req.body
+    const exp = new Expense({ amount: amount, date: date, description: description, category: category, account: account })
+    await exp.save()
+
+    await calculation(account, amount)
+
+    res.status(201).json(exp)
+
+  } catch (err) {
+    res
+      .status(400)
+      .json({ message: 'Could not add expense', errors: err.errors })
+  } 
+})
+
+// recalculate saldo (accounts)
+
+const calculation = async (accountId, amount) => {
+  const accToC = await Account.findById(accountId)
+  accToC.saldo = accToC.saldo + amount
+
+  await accToC.save()
+}
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}`)
+})
